98241d31792699c96844f11b47ad7687ea023884
suryamgupta25 <suryamg13@gmail.com>
2024-09-13 19:13:36 -0400

    fine tuned stock predictions on apple

--- train.py
+++ train.py
@@ -10,19 +10,34 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.layers import Dense
 
-trained_models = []
+from matplotlib.backend_bases import MouseButton
+
+trained_models = {}
+
+def train_stock(name:str):
+    data = open(name + ".csv", 'r')
+    pass
+
+def test_stock(name:str, dataset):
+    pass
+
+"""
+def on_move(event):
+    if event.inaxes:
+        print(f'data coords {event.xdata} {event.ydata},',
+"""
 
 if __name__ == "__main__":
-    reader = open('apple_data.csv', 'r')
-    data = pd.read_csv('apple_data.csv')
+    data = pd.read_csv('AAPL.csv')
 
-    dataset = data[['Date', 'Open', 'Close/Last']]
+    dataset = data[['Date', 'Open', 'Close/Last', 'Volume']]
     # Convert date strings to Datetime
     for i, j in dataset.iterrows():
         j['Date'] = datetime.strptime(j['Date'], '%m/%d/%Y') # %Y is for 4-digit years, %y is for 2-digit years
     dataset = dataset.sort_values(by=['Date']).rename(columns={'Close/Last': 'Close'})
     dataset['Open'] = dataset['Open'].str[1:].astype(float).round(2)
     dataset['Close'] = dataset['Close'].str[1:].astype(float).round(2)
+    dataset['Volume'] = dataset['Volume'].astype(float)
 
     num_rows_training = math.floor(0.8 * len(dataset))
     num_rows_testing = len(dataset) - num_rows_training
@@ -34,14 +49,14 @@ if __name__ == "__main__":
     train_set_scaled = train_set.copy()
     max_val = max(train_set['Open'].max(), train_set['Close'].max())
     min_val = min(train_set['Open'].min(), train_set['Close'].min())
+    max_volume = train_set['Volume'].max()
+    min_volume = train_set['Volume'].min()
     train_set_scaled['Open'] = (train_set_scaled['Open'] - min_val) / (max_val - min_val)
     train_set_scaled['Close'] = (train_set_scaled['Close'] - min_val) / (max_val - min_val)
+    train_set_scaled['Volume'] = (train_set_scaled['Volume'] - min_volume) / (max_volume - min_volume)
     
-    #train_set_scaled['Open'].plot(kind='line')
-    #train_set_scaled['Close'].plot(kind='line')
-    #plt.show()
 
-    #print(train_set_scaled.head().to_numpy())
+    print(train_set_scaled.head().to_numpy())
     X_train = []
     Y_train = []
     for i,j in train_set_scaled.iterrows():
@@ -51,23 +66,29 @@ if __name__ == "__main__":
     X_train = np.array(X_train)
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     Y_train = np.array(Y_train)
-
-    # Building model
-    dropout_val = 0.15
-    model = Sequential()
-    model.add(LSTM(units=128,return_sequences=True,input_shape=(X_train.shape[1], 1)))
-    model.add(Dropout(dropout_val))
-    model.add(LSTM(units=64,return_sequences=True))
-    model.add(Dropout(dropout_val))
-    model.add(LSTM(units=32,return_sequences=True))
-    model.add(Dropout(dropout_val))
-    model.add(LSTM(units=16, return_sequences=True))
-    model.add(Dropout(dropout_val))
-    model.add(LSTM(units=8))
-    model.add(Dropout(dropout_val))
-    model.add(Dense(units=1))
-    model.compile(optimizer='adam',loss='mean_squared_error')
-    model.fit(X_train,Y_train,epochs=100,batch_size=4)
+    
+    
+    if 'AAPL' not in trained_models:
+        # Building model
+        unit_size = 75
+        dropout_val = 0.10
+        model = Sequential()
+        model.add(LSTM(units=unit_size,return_sequences=True,input_shape=(X_train.shape[1], 1)))
+        model.add(Dropout(dropout_val))
+        model.add(LSTM(units=unit_size,return_sequences=True))
+        model.add(Dropout(dropout_val))
+        model.add(LSTM(units=unit_size,return_sequences=True))
+        model.add(Dropout(dropout_val))
+        model.add(LSTM(units=unit_size, return_sequences=True))
+        model.add(Dropout(dropout_val))
+        model.add(LSTM(units=unit_size))
+        model.add(Dropout(dropout_val))
+        model.add(Dense(units=1))
+        model.compile(optimizer='adam',loss='mean_squared_error')
+        model.fit(X_train,Y_train,epochs=100,batch_size=16)
+        trained_models['AAPL'] = model
+    
+    trained_model = trained_models['AAPL']
 
     # Testing the model
     test_dataset = pd.concat((train_set['Open'], test_set['Open']), axis=0)
@@ -83,7 +104,7 @@ if __name__ == "__main__":
     inputs = np.array(inputs)
     inputs = np.reshape(inputs, (inputs.shape[0], inputs.shape[1], 1))
 
-    testing_results = model(inputs)
+    testing_results = trained_model(inputs)
     testing_results = testing_results.numpy()
     testing_results = np.array(testing_results)
 
@@ -107,7 +128,7 @@ if __name__ == "__main__":
     testingAccurateTrends = 0
     for i in range(num_rows_training, len(dataset) - 1, 1):
         actualUp = actual_values[i + 1] - actual_values[i] >= 0
-        predictUp = final_predictions[i+1] - final_predictions[i] >= 0
+        predictUp = final_predictions[i+1] - final_predictions[i] > 0
         if actualUp == predictUp:
             testingAccurateTrends += 1
     print("Testing Accuracy: {accuracy:.2f}%\n".format(accuracy=(testingAccurateTrends / (len(dataset) - num_rows_training - 1) * 100)))
@@ -122,7 +143,10 @@ if __name__ == "__main__":
     plt.plot(x_axis, actual_values, label='Actual Stock Price')
     plt.plot(final_predictions, label='Predicted Price')
     plt.title("Apple Stock Price Prediction")
-    plt.xlabel("Days since August 7, 2023")
+    plt.xlabel("Days since {date:s}".format(date=dataset['Date'][0]))
     plt.ylabel("Stock Price (USD)")
     plt.legend()
+
+    #binding_id = plt.connect('motion_notify_event', on_move)
+
     plt.show()
